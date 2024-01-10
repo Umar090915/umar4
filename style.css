d5872f855b484de3ff0ffccf256eb372d4f97345
Umar090915 <155972531+Umar090915@users.noreply.github.com>
2024-01-10 17:30:20 +0600

    Create style.css

---
+++ style.css
@@ -0,0 +1,115 @@
+body {
+    font-family: sans-serif;
+}
+.g{
+    background-color: lightgrey;
+    background-image: url("https://www.creativefabrica.com/wp-content/uploads/2021/11/02/Abstract-cinema-logo-vector-template-Graphics-19545950-1-580x386.png");
+    width: 200px;
+    height: 100px;
+    margin: 0 auto;
+}
+.y{
+    background-image: url("https://static.tildacdn.com/tild6165-3737-4164-a634-346531393562/netflix_app_icon.jpg");
+}
+.o{
+    background-image: url("https://play-lh.googleusercontent.com/lMoItBgdPPVDJsNOVtP26EKHePkwBg-PkuY9NOrc-fumRtTFP4XhpUNk_22syN4Datc")
+}
+body{
+    background-color: lightgray;
+}
+a{
+    border: black double;
+    color: black;
+    font-size: 25px;
+}
+a:hover{
+    border: red double;
+}
+header {
+    display: flex;
+    align-items: center;
+  
+}
+
+header p {
+    font-size: 30px;
+    color: white;
+}
+
+header img {
+    width: 65px;
+    height: 55px;
+    padding: 15px;
+}
+
+h1 {
+    color: #B40101;
+    font-size: 30px;
+    text-align: center;
+}
+
+body {
+    background-color: #D9D9D9;
+}
+
+.genres {
+    padding-bottom: 15px;
+padding-top: 15px;
+background: #9A2424;
+}
+
+.genres a {
+    font-size: 20px;
+color: #FFFFFF;
+padding-left: 15px;
+padding-right: 40px;
+    text-align: center;
+}
+
+.film a {
+ display: block;
+ font-size: 20px;
+ color:#9A2424;
+ text-align: center;
+}
+
+.film {
+    background-color:;
+    justify-content: center;
+    margin: 20px;
+}
+
+section{
+    display: flex;
+    justify-content: center;
+    border-bottom: 10px solid black;
+}
+
+h2 {
+    text-align: center;
+    align-items: center;
+}
+
+
+footer {
+    background-color: #9A2424;
+}
+.t{
+    background-color: burlywood;
+}
+.o{
+    background-color: burlywood;
+}
+
+li {
+    margin: 10px;
+}
+
+iframe {
+    display: block;
+    margin: 0 auto;
+}
+
+h3 {
+    text-align: center;
+}
